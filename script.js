a8079f9a67e4dfdbd408f3f7746a7da817f4241f
Helena-Alaster <93897372+Helena-Alaster@users.noreply.github.com>
2026-09-01 03:33:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
+
+// Отключаем контекстное меню (правая кнопка мыши)
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
+
+// Отключаем копирование по Ctrl+C / Cmd+C
+document.addEventListener('keydown', function(event) {
+    if ((event.ctrlKey || event.metaKey) && (event.key === 'c' || event.key === 'C')) {
+        event.preventDefault(); // Предотвращаем стандартную реакцию
+    }
+});
